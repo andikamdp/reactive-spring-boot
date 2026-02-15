3038836e98342ae6185019bc37980b5a3ca904d9
ranmuh <dikaprapmanto@gmail.com>
2026-02-15 08:34:23 +0700

    git init project

---
+++ src/test/java/id/co/practice/reactivespringboot/ReactiveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.practice.reactivespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
